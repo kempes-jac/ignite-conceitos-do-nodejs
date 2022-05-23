50d89bb895ae3e0fb75f55566dfc4f6e59a74d34
Kempes J <kempes.jac@gmail.com>
2022-05-23 14:53:33 -0300

    Updated version - refactored from Rocketseat template

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ignite NodeJS - Chapter 1 Main Challenge (PT-BR)
+
+Este projeto é parte da Trilha NodeJS,  curso de treinamento de NodeJS da RocketSeat (rocketseat.com.br). Este é o primeiro desafio da trilha.
+
+O resultado do projeto deve ser um *beck-end* escrito em NodeJS. Esse *beck-end* deve implementar uma API para lidar com usuários e suas *todo lists* (listas de tarefas).
+
+O *beck-end* deve ter *endpoints* para lidar com cadastro de usuários e CRUD das *todos* .
+
+Para agilizar os testes, o projeto deve ter testes automatizados com Jest.
+
+# Ignite NodeJS - Chapter 1 Main Challenge (EN-US)
+
+This project is part of the NodeJs Trail, RocketSeat's (rocketseat.com.br) NodeJS training course. This is the first challenge of the the trail.
+
+The result of this project must be a beck end written in NodeJS. This beck end must implement an API to handle users and their *todo lists*.
+
+The beck end must have end points to deal with users registration and *todos* CRUD.
+
+To speed up tests, the project must have automated tests with Jest.
--- src/index.js
+++ src/index.js
@@ -1,41 +1,151 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  //Get username from request.headers
+  const { username } = request.headers;
+  //Get user from users list
+  const user = users.find( user => user.username == username );
+  //Check if user exists
+  if(!user){
+    return response.status(404).json({error: 'Mensagem do erro'})
+  }
+
+  //If the user exists, its data is added to request (it may be uses later)
+  request.user = user;
+  //Next function call
+  return next();
 }
 
+
+/**
+ * Check if task exist, trying to find it inside user todos list, using id
+ * sent via request params
+ * @param {*} request HTTP request
+ * @param {*} response HTTP response
+ * @param {*} next Next function call in chain
+ * @returns An error message, if task does not exist, or next function call
+ */
+ const checkExistsTask = (request, response, next ) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  //Task retrieval
+  const task  = user.todos.find( todo => todo.id === id );
+
+  //Task validation
+  if(!task){
+    return response.status(404).json({error: 'Mensagem do erro'})
+  }
+
+  request.task = task;
+
+  return next();
+
+};
+
+
+
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const user = request.body;
+
+  //Check if users already exists
+  const userAlreadyExists = users.some( oldUser => 
+    oldUser.username === user.username );
+
+  //If the sent users already exists, a error is returned
+  if(userAlreadyExists){
+    return response.status(400).json({error: 'Mensagem do erro'})
+  }
+
+  //If user does not exists, a new one is created using a new UUID and
+  //an empty todo list
+  const newUser = {
+    id: uuidv4(),
+    name: user.name,
+    username: user.username,
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  //Task data comes inside request body as a JSON content.
+  const { title, deadline } = request.body;
+
+  //A new task is created using a new UUID and current date and time
+  const newTodo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(newTodo);
+  //After created, a 200 status is returned
+  return response.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, checkExistsTask, (request, response) => {
+  //New data sent via request body
+  const { title, deadline } = request.body;
+  
+  const { task } = request;
+
+  //New title validation
+  if(title){
+    task.title = title;
+  }
+
+  //New deadline validation
+  if(deadline){
+    const newDeadLine = new Date(deadline);
+    if(newDeadLine != "Invalid Date"){
+      task.deadline = newDeadLine;
+    }else{
+      return response.status(400).json({error:"Invalid date"})
+    }
+  }
+
+  //OK response
+  return response.status(200).json(task);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, checkExistsTask, (request, response) => {
+  const { task } = request;
+
+  task.done = true;
+
+  return response.status(200).json(task);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, checkExistsTask, (request, response) => {
+  const { user } = request;
+  const { task } = request;
+
+  user.todos.splice(user.todos.indexOf(task),1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
